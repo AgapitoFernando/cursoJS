d7aec254e0d20238667fa5fc50345f6ee0244e11
Fernando Agapito da Veiga <nandoagapito83@gmail.com>
2025-09-02 16:30:15 -0300

    Update script.js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora <12) {
+        //Bom dia!
+        img.src = 'Alvorada (1).png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >=12 && hora <18) {
+        //Boa tarde!
+        img.src = 'Entardecer (1).png'
+        document.body.style.background = '#e7650eff'
+    } else {
+        //Boa noite!
+        img.src = 'Noite(1).png'
+        document.body.style.background = '#515154'
+    }
+}
