57315978ce8554d361f08b3a45e0614568e9829d
Agapito Fernando <nandoagapito83@gmail.com>
2025-09-29 19:44:36 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,5 +2,51 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fAno = document.getElementById('txtAno')
-    var res = document.getElementById('res')
+    var res = document.querySelector('div#res')
+
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fSex = document.getElementsByName('radSex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fSex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fSex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 60) {
+                // Adulta
+                img.setAttribute('src', 'foto-adulta-f.png')
+            } else {
+                //Idosa
+                img.setAttribute('src', 'foto-idosa-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
 }
+
+// Colocar as fotos na mesma pasta! Trabalho de casa
